6fa2a744b3de376d198cb8e81bc0c3ee1bcb00bd
fejszesbence <fejszesbence@gmail.com>
2021-02-12 12:20:00 +0100

    második feladat

--- src/fuvar/Fuvar.java
+++ src/fuvar/Fuvar.java
@@ -1,11 +1,30 @@
 package fuvar;
 
-public class Fuvar {
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
-    public static void main(String[] args) {
+public class Fuvar {
+    
+    private int taxiAzon;
+    private String indulas;
+    private int idotartam;
+    private double tavolsag, viteldij, borravalo;
+    private String fizmod;
+
+    public Fuvar(String sor) throws ParseException {
+        NumberFormat formatum = NumberFormat.getInstance(Locale.getDefault());
         
-        
-        
+        String[] darabok = sor.split(";");
+        this.taxiAzon = Integer.parseInt(darabok[0]);
+        this.indulas = darabok[1];
+        this.idotartam = Integer.parseInt(darabok[2]);
+        this.tavolsag = formatum.parse(darabok[3]).doubleValue();
+        this.viteldij = formatum.parse(darabok[4]).doubleValue();
+        this.borravalo = formatum.parse(darabok[5]).doubleValue();
+        this.fizmod = darabok[6];
     }
+    
+    
     
 }
---
+++ src/fuvar/FuvarProgram.java
@@ -0,0 +1,23 @@
+package fuvar;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FuvarProgram {
+
+    public static void main(String[] args) throws IOException, ParseException {
+        
+        List<String> sorok = Files.readAllLines(Paths.get("fuvar.csv"));
+        ArrayList<Fuvar> fuvarok = new ArrayList<>();
+        for (int i = 1; i < sorok.size(); i++) {
+            fuvarok.add(new Fuvar(sorok.get(i)));
+        }
+        
+    }
+    
+}
